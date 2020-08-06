ef03fc2a54e4541aa884e0397823aee2308881f8
Neil Yonzon 2 <ning.yonzon@gmail.com>
2020-08-06 02:35:06 -0700

    Configured Menu Walker

--- functions.php
+++ functions.php
@@ -54,6 +54,56 @@ if ( ! function_exists( 'datacon_refresh_setup' ) ) :
 			)
 		);
 
+		// Create Class to extend Walker_Nav_Menu class to customize/override Menu
+
+		
+
+		class Walker_Nav_Primary extends Walker_Nav_Menu {
+			function start_lvl( &$output, $depth = 0, $args = array()) { //ul
+				//Variable that is used to create indents in the HTML code
+				$indent = str_repeat("\t",$depth);
+				$submenu = ($depth > 0 ) ? 'subnav': '';
+				$output  .= "\n$indent<ul class=\"subnav__nav$submenu depth_$depth\">\n";
+			}
+
+			function start_el(&$output, $item, $depth = 0, $args = array(), $current_object_id = 0) { //li a span (manages the markup of the li a span)
+				$indent = ($depth) ? str_repeat("\t",$depth) : '';
+
+				$li_attributes = '';
+				$class_names = $value = '';
+
+				$classes = empty($item -> classes) ? 'array()' : (array) $item -> classes;
+				
+				$classes[] = ($args -> walker-> has_children) ? 'subnav' : '';
+				$classes[] = ($item -> current || $item -> current_item_ancestor) ? 'active' : '';
+				$classes[] = 'nav__item';
+		
+
+				$class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
+				$class_names = 'class = "' . esc_attr($class_names) . '"';
+				// $id = apply_filters('nav_menu_item_id', 'menu-item-'. $item-> ID, $item, $args);
+				// $id = strlen( $id ) ? ' id="' . esc_attr($id) . '"' : '';
+				$output .= $indent . '<li ' . $value . $class_names . $li_attributes . '>';
+				$attributes = ! empty( $item -> attr_title ) ? ' title = "' . esc_attr($item -> attr_title) . '"' : '';
+				$attributes .= ! empty ($item -> target) ? ' target= "' . esc_attr($item -> target) . '"' : '';
+				$attributes .= ! empty ($item -> xfn) ? ' rel= "' . esc_attr($item -> xfn) . '"' : '';
+				$attributes .= ! empty ($item -> url) ? ' href= "' . esc_attr($item -> url) . '"' : '';
+				$attributes .= 'class = "nav__link"';
+				$item_output  = $args-> before;
+				$item_output .= '<a' . $attributes .'>';
+				$item_output .= $args-> link_before . apply_filters( 'the_title', $item -> title, $item-> ID) . $args -> link_after;
+				$item_output .= ($depth == 0 && $args-> walker-> has_children) ? '</a>
+				<svg class="subnav__icon">
+					<use
+						xlink:href="https://data-con-la.local/wp-content/themes/datacon-refresh/img/sprite.svg#icon-chevron-thin-down">
+					</use>
+				</svg>' : '</a>';
+				$item_output .= $args -> after;
+
+				$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+			}
+		}
+
 		/*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
--- header.php
+++ header.php
@@ -28,13 +28,6 @@
             href="#primary"><?php esc_html_e( 'Skip to content', 'datacon-refresh' ); ?></a>
 
         <header id="masthead" class="site-header">
-            <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'main-nav',
-            )
-        )
-        ?>
             <nav id="site-navigation" class="main-navigation">
                 <div class="main-navigation__logo">
                     <a href="#" class="main-navigation__logo-link">
@@ -42,25 +35,14 @@
                             class="main-navigation__logo-img">
                     </a>
                 </div>
-                <ul class="nav">
-                    <li class="nav__item subnav"><a href="#" class="nav__link">Conference</a>
-                        <svg class="subnav__icon">
-                            <use
-                                xlink:href="<?php echo get_stylesheet_directory_uri() ?>/img/sprite.svg#icon-chevron-thin-down">
-                            </use>
-                        </svg>
-                        <ul class="subnav__nav">
-                            <li class="nav__item"><a href="#" class="nav__link">Sublink</a>
-                            <li class="nav__item"><a href="#" class="nav__link">Sublink Sublink</a>
-                            <li class="nav__item"><a href="#" class="nav__link">Sublindfsdfds</a>
-                        </ul>
-                    </li>
-                    <li class="nav__item"><a href="#" class="nav__link">Agenda</a></li>
-                    <li class="nav__item"><a href="#" class="nav__link">Speakers</a></li>
-                    <li class="nav__item"><a href="#" class="nav__link">Sponsors</a></li>
-                    <li class="nav__item"><a href="#" class="nav__link">Panels</a></li>
-                    <li class="nav__item"><a href="#" class="nav__link">Startup Showcase</a></li>
-                    <li class="nav__item"><a href="#" class="nav__link">Get Tickets</a></li>
-                </ul>
+                <?php
+                       wp_nav_menu(array(
+                            'theme_location'  => 'main-nav',
+                            'container'       => 'ul',
+                            'menu_class'      => 'nav',
+                            'walker'          => new Walker_Nav_Primary()
+                            )
+                       );
+                ?>
             </nav>
         </header><!-- #masthead -->
