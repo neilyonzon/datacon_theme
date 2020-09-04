6719f0c9fcf745d5b97fbcf5e448cf0d470b83ab
Neil Yonzon 2 <ning.yonzon@gmail.com>
2020-09-04 00:19:05 -0700

    Completed Two Column Component

--- inc/function-wpb-components.php
+++ inc/function-wpb-components.php
@@ -20,6 +20,7 @@ function hp_hero_integrateWithVC()
                     "param_name" => "header_1",
                     "value" => __("Heading 1", "my-text-domain"),
                     "description" => __("First Line of H1 Element.", "my-text-domain"),
+                    'save_always' => true,
                 ),
                 array(
                     "type" => "textfield",
@@ -29,6 +30,7 @@ function hp_hero_integrateWithVC()
                     "param_name" => "header_2",
                     "value" => __("Heading 2", "my-text-domain"),
                     "description" => __("Second Line of H1 Element (optional)", "my-text-domain"),
+                    'save_always' => true,
                 ),
                 array(
                     "type" => "textfield",
@@ -38,6 +40,7 @@ function hp_hero_integrateWithVC()
                     "param_name" => "header_3",
                     "value" => __("Heading 3", "my-text-domain"),
                     "description" => __("Third Line of H1 Element (optional).", "my-text-domain"),
+                    'save_always' => true,
                 ),
                 array(
                     "type" => "textarea",
@@ -47,15 +50,17 @@ function hp_hero_integrateWithVC()
                     "param_name" => "sub_heading", // Important: Only one textarea_html param per content element allowed and it should have "content" as a "param_name"
                     "value" => __("Enter Text", "my-text-domain"),
                     "description" => __("Enter subheading content.", "my-text-domain"),
+                    'save_always' => true,
                 ),
                 array(
                     "type" => "textarea_html",
                     "holder" => "div",
                     "class" => "",
                     "heading" => __("Description", "my-text-domain"),
-                    "param_name" => "description", // Important: Only one textarea_html param per content element allowed and it should have "content" as a "param_name"
+                    "param_name" => "content", // Important: Only one textarea_html param per content element allowed and it should have "content" as a "param_name"
                     "value" => __("<p>I am test text block. Click edit button to change this text.</p>", "my-text-domain"),
                     "description" => __("Enter your content.", "my-text-domain"),
+                    'save_always' => true,
                 ),
                 array(
                     "type" => "vc_link",
@@ -65,6 +70,7 @@ function hp_hero_integrateWithVC()
                     "param_name" => "cta",
                     "value" => __("Enter Link", "my-text-domain"),
                     "description" => __("Enter link for call to action button.", "my-text-domain"),
+                    'save_always' => true,
                 ),
                 array(
                     "type" => "attach_image",
@@ -74,6 +80,7 @@ function hp_hero_integrateWithVC()
                     "param_name" => "bg_image",
                     'admin_label' => true,
                     "description" => __("Choose image for background.", "my-text-domain"),
+                    'save_always' => true,
                 ),
             ),
         )
@@ -81,14 +88,13 @@ function hp_hero_integrateWithVC()
 }
 
 add_shortcode('hero_banner', 'output_hp_hero');
-function output_hp_hero($atts, $content = null)
+function output_hp_hero($atts, $content, $tag)
 {
-    $content = wpb_js_remove_wpautop($content, true); // fix unclosed/unwanted paragraph tags in $content
+    $atts = vc_map_get_attributes($tag, $atts);
     $header1 = esc_attr($atts['header_1']);
     $header2 = esc_attr($atts['header_2']);
     $header3 = esc_attr($atts['header_3']);
     $sub_heading = esc_attr($atts['sub_heading']);
-    $description = wpb_js_remove_wpautop($atts['description'], true);
     $cta = vc_build_link($atts['cta']);
     $image_src = wp_get_attachment_image_src($atts['bg_image'], 'full')[0];
     $image_alt = get_post_meta($atts['bg_image'], '_wp_attachment_image_alt', true);
@@ -122,8 +128,8 @@ function output_hp_hero($atts, $content = null)
     $output .= '</div>';
 
     $output .= '<div class="hero__description">';
-    if ($description != '') {
-        $output .= $description;
+    if ($content != '') {
+        $output .= $content;
     }
     $output .= '</div>';
     if ($cta['url'] != '') {
@@ -191,12 +197,20 @@ function output_callout_desc($atts, $content, $tag)
     $callout = $atts['callout'];
     $calloutColor = $atts['callout-color'];
     $description = $content;
+    //start of parent div
     $output .= "<div class=\"container-flex\">";
+
+    //start of child div
     $output .= "<div class=\"col-1-of-2\">";
     $output .= "<div class=\"callout {$calloutColor}\">{$callout}</div>";
-    $output .= "{$calloutColor}";
-    $output .= $description;
     $output .= "</div>";
+
+    //start of child div
+    $output .= "<div class=\"col-1-of-2 flex-align--center headspace-s--m\">";
+    $output .= "<p>$description</p>";
+    $output .= "</div>";
+
+    //start of parent div
     $output .= "</div>";
     return $output;
 }
