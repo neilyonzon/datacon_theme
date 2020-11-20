89c5d734db90863e263900f9125effd83776056c
Neil Yonzon 2 <ning.yonzon@gmail.com>
2020-11-20 00:59:08 -0800

    Configured Interior Banner

--- page.php
+++ page.php
@@ -16,21 +16,25 @@ get_header();
 ?>
 <main id="primary" class="main-content">
 
-    <?php if (has_post_thumbnail($post->ID)):
-    $image_id = get_post_thumbnail_id();
-    $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', true);
-    $size = 'my-size'; // Defaults to 'thumbnail' if omitted.
-    $image_src = wp_get_attachment_image_src($image_id, $size)[0];
-    ?>
+    <?php
+$image_id = get_post_thumbnail_id();
+$image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', true);
+$size = 'my-size'; // Defaults to 'thumbnail' if omitted.
+$image_src = wp_get_attachment_image_src($image_id, $size)[0];
+if (get_field('hero_banner_color') == 'Blue') {
+    $hero_color = 'primary';
+} else {
+    $hero_color = 'secondary';
+}
+;?>
     <div class="interior-banner">
         <h1 class="interior-banner__header"><?php single_post_title();?></h1>
-        <img class="interior-banner__img" src="<?php echo $image_src; ?>" alt="<?php echo $image_alt ?>" />
+        <?php if (has_post_thumbnail($post->ID)): ?>
+        <img class="interior-banner__img" src="<?php echo $image_src; ?>" alt="<?php echo $image_alt; ?>" />
+        <?php endif;?>
+        <div class="interior-banner__overlay interior-banner__overlay--<?php echo $hero_color; ?>"></div>
     </div>
-    <?php else: ?>
-    <div class="interior-banner">
-        <h1 class=" interior-banner__header"><?php single_post_title();?></h1>
-    </div>
-    <?php endif;
+    <?php
 while (have_posts()):
     the_post();
 
