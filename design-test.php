b45031819c6515147152890114ca8d9c6022338d
Neil Yonzon <neilyonzon@Lubbys-Laptop.lan>
2020-12-15 02:29:41 -0800

    Added CSS for grid container

--- design-test.php
+++ design-test.php
@@ -38,12 +38,13 @@ get_header();
     <div class="section-container">
         <div class="profile-grid">
             <div class="profile-item">
-                <a href="https://www.facebook.com/"><img class="profile-item__img" src="https://via.placeholder.com/300"
+                <a href="https://www.facebook.com/"><img class="profile-item__img"
+                        src="https://www.dataconla.com/wp-content/uploads/2019/07/Annie_linkedin_small-Annie-Flippo-e1562092059167.jpg"
                         alt="placeholder" /></a>
                 <a href="https://www.facebook.com/" class="profile-item__name">
-                    <h4>Jennifer Conley Lee</h4>
+                    <h4>Jennifer Conley Lee Lee</h4>
                 </a>
-                <p>Web Developer</p>
+                <p class="profile-item__title">Web Developer Developer Developer</p>
                 <div class="profile-item__social-icons-container">
                     <div class="profile-item__social-icon">
                         <a href="https://www.facebook.com/" class="profile-item__social-link">
@@ -54,8 +55,143 @@ get_header();
                         </a>
                     </div>
                     <div class="profile-item__social-icon">
+                        <a href="https://www.twitter.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH?>/sprite.svg#icon-twitter-with-circle"></use>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="profile-item">
+                <a href="https://www.facebook.com/"><img class="profile-item__img"
+                        src="https://www.dataconla.com/wp-content/uploads/2020/10/Nilesh-Tiloda.jpg"
+                        alt="placeholder" /></a>
+                <a href="https://www.facebook.com/" class="profile-item__name">
+                    <h4 class="profile-item__name">Nilesh Tiloda</h4>
+                </a>
+                <p class="profile-item__title">Director Of Partnerships & Field Operations At Helping Hands Community
+                </p>
+                <div class="profile-item__social-icons-container">
+                    <div class="profile-item__social-icon">
                         <a href="https://www.facebook.com/" class="profile-item__social-link">
-                            <svg class="profile-grid__social-symbol">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH ?>/sprite.svg#icon-linkedin-with-circle">
+                                </use>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="profile-item__social-icon">
+                        <a href="https://www.twitter.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH?>/sprite.svg#icon-twitter-with-circle"></use>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="profile-item">
+                <a href="https://www.facebook.com/"><img class="profile-item__img"
+                        src="https://www.dataconla.com/wp-content/uploads/2020/10/Nilesh-Tiloda.jpg"
+                        alt="placeholder" /></a>
+                <a href="https://www.facebook.com/" class="profile-item__name">
+                    <h4 class="profile-item__name">Nilesh Tiloda</h4>
+                </a>
+                <p class="profile-item__title">Director Of Partnerships & Field Operations At Helping Hands Community
+                </p>
+                <div class="profile-item__social-icons-container">
+                    <div class="profile-item__social-icon">
+                        <a href="https://www.facebook.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH ?>/sprite.svg#icon-linkedin-with-circle">
+                                </use>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="profile-item__social-icon">
+                        <a href="https://www.twitter.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH?>/sprite.svg#icon-twitter-with-circle"></use>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="profile-item">
+                <a href="https://www.facebook.com/"><img class="profile-item__img"
+                        src="https://www.dataconla.com/wp-content/uploads/2020/10/Nilesh-Tiloda.jpg"
+                        alt="placeholder" /></a>
+                <a href="https://www.facebook.com/" class="profile-item__name">
+                    <h4 class="profile-item__name">Nilesh Tiloda</h4>
+                </a>
+                <p class="profile-item__title">Director Of Partnerships & Field Operations At Helping Hands Community
+                </p>
+                <div class="profile-item__social-icons-container">
+                    <div class="profile-item__social-icon">
+                        <a href="https://www.facebook.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH ?>/sprite.svg#icon-linkedin-with-circle">
+                                </use>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="profile-item__social-icon">
+                        <a href="https://www.twitter.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH?>/sprite.svg#icon-twitter-with-circle"></use>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="profile-item">
+                <a href="https://www.facebook.com/"><img class="profile-item__img"
+                        src="https://www.dataconla.com/wp-content/uploads/2020/10/Nilesh-Tiloda.jpg"
+                        alt="placeholder" /></a>
+                <a href="https://www.facebook.com/" class="profile-item__name">
+                    <h4 class="profile-item__name">Nilesh Tiloda</h4>
+                </a>
+                <p class="profile-item__title">Director Of Partnerships & Field Operations At Helping Hands Community
+                </p>
+                <div class="profile-item__social-icons-container">
+                    <div class="profile-item__social-icon">
+                        <a href="https://www.facebook.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH ?>/sprite.svg#icon-linkedin-with-circle">
+                                </use>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="profile-item__social-icon">
+                        <a href="https://www.twitter.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH?>/sprite.svg#icon-twitter-with-circle"></use>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="profile-item">
+                <a href="https://www.facebook.com/"><img class="profile-item__img"
+                        src="https://www.dataconla.com/wp-content/uploads/2020/10/Nilesh-Tiloda.jpg"
+                        alt="placeholder" /></a>
+                <a href="https://www.facebook.com/" class="profile-item__name">
+                    <h4 class="profile-item__name">Nilesh Tiloda</h4>
+                </a>
+                <p class="profile-item__title">Director Of Partnerships & Field Operations At Helping Hands Community
+                </p>
+                <div class="profile-item__social-icons-container">
+                    <div class="profile-item__social-icon">
+                        <a href="https://www.facebook.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
+                                <use xlink:href="<?php echo THEME_IMG_PATH ?>/sprite.svg#icon-linkedin-with-circle">
+                                </use>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="profile-item__social-icon">
+                        <a href="https://www.twitter.com/" class="profile-item__social-link">
+                            <svg class="profile-item__social-symbol">
                                 <use xlink:href="<?php echo THEME_IMG_PATH?>/sprite.svg#icon-twitter-with-circle"></use>
                             </svg>
                         </a>
