21b98e3c63c549b17422c501857fb2f630c61c94
Neil Yonzon <neilyonzon@Lubbys-Laptop.lan>
2021-04-06 01:10:55 -0700

    Added HTML structure for Listing Feature Component

---
+++ design-listing-feature.php
@@ -0,0 +1,67 @@
+<?php 
+
+/* Template Name: Design Test Listings Feature */
+
+/**
+ * The template for displaying all landing page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Datacon_Refresh
+ */
+
+get_header();
+?>
+
+<main id="primary" class="main-content">
+    <?php ?>
+
+    <div class="listing-feature">
+        <img class="listing-feature__img" src="" alt="" />
+        <div class="listing-feature__overlay listing-feature__overlay--secondary"></div>
+        <div class="listing-feature__media">
+            <h3 class="listing-feature__header header">Keynote Speakers</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.</p>
+            <img class="listing-feature__img" src="https://placekitten.com/450/300" alt="placeholder" />
+        </div>
+        <hr class="line" />
+        <div class="listing-feature__list">
+            <div class="listing-feature__item">
+                <span class="listing-feature__item-name">Subash D'Souza</span>
+                <span class="listing-feature__item-title">Founder, Data Con LA</span>
+            </div>
+            <div class="listing-feature__item">
+                <span class="listing-feature__item-name">Subash D'Souza</span>
+                <span class="listing-feature__item-title">Founder, Data Con LA</span>
+            </div>
+            <div class="listing-feature__item">
+                <span class="listing-feature__item-name">Subash D'Souza</span>
+                <span class="listing-feature__item-title">Founder, Data Con LA</span>
+            </div>
+            <div class="listing-feature__item">
+                <span class="listing-feature__item-name">Subash D'Souza</span>
+                <span class="listing-feature__item-title">Founder, Data Con LA</span>
+            </div>
+            <div class="listing-feature__item">
+                <span class="listing-feature__item-name">Subash D'Souza</span>
+                <span class="listing-feature__item-title">Founder, Data Con LA</span>
+            </div>
+            <div class="listing-feature__item">
+                <span class="listing-feature__item-name">Subash D'Souza</span>
+                <span class="listing-feature__item-title">Founder, Data Con LA</span>
+            </div>
+        </div>
+    </div>
+
+
+
+</main>
+
+<?php
+get_footer();
