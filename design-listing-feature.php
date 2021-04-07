21c1e3da0dc5a85583276c486b4351af38a950c2
Neil Yonzon <neilyonzon@Lubbys-Laptop.lan>
2021-04-06 23:31:47 -0700

    Updated CSS for listing feature

--- design-listing-feature.php
+++ design-listing-feature.php
@@ -19,10 +19,9 @@ get_header();
 ?>
 
 <main id="primary" class="main-content">
-    <?php ?>
 
     <div class="listing-feature">
-        <img class="listing-feature__img" src="" alt="" />
+        <img class="listing-feature__bg" src="<?php echo THEME_IMG_PATH  ?>/data_graphic.jpg" alt="Graphic" />
         <div class="listing-feature__overlay listing-feature__overlay--secondary"></div>
         <div class="listing-feature__media">
             <h3 class="listing-feature__header header">Keynote Speakers</h3>
@@ -32,29 +31,62 @@ get_header();
         </div>
         <hr class="line" />
         <div class="listing-feature__list">
-            <div class="listing-feature__item">
-                <span class="listing-feature__item-name">Subash D'Souza</span>
-                <span class="listing-feature__item-title">Founder, Data Con LA</span>
-            </div>
-            <div class="listing-feature__item">
-                <span class="listing-feature__item-name">Subash D'Souza</span>
-                <span class="listing-feature__item-title">Founder, Data Con LA</span>
-            </div>
-            <div class="listing-feature__item">
-                <span class="listing-feature__item-name">Subash D'Souza</span>
-                <span class="listing-feature__item-title">Founder, Data Con LA</span>
-            </div>
-            <div class="listing-feature__item">
-                <span class="listing-feature__item-name">Subash D'Souza</span>
-                <span class="listing-feature__item-title">Founder, Data Con LA</span>
-            </div>
-            <div class="listing-feature__item">
-                <span class="listing-feature__item-name">Subash D'Souza</span>
-                <span class="listing-feature__item-title">Founder, Data Con LA</span>
-            </div>
-            <div class="listing-feature__item">
-                <span class="listing-feature__item-name">Subash D'Souza</span>
-                <span class="listing-feature__item-title">Founder, Data Con LA</span>
+            <div class="listing-feature__list-container">
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">Subash D'Souza</span>
+                    <span class="listing-feature__item-title">Founder, Data Con LA</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">ABBASS SHARIF, PHD.</span>
+                    <span class="listing-feature__item-title">Founder, Data Con LA</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">ANITA SENGUPTA</span>
+                    <span class="listing-feature__item-title">Chief Product Officer / Co-Founder at at Airspace
+                        Experience Technologies</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">Subash D'Souza</span>
+                    <span class="listing-feature__item-title">Founder, Data Con LA</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">ABBASS SHARIF, PHD.</span>
+                    <span class="listing-feature__item-title">Academic Director, Master of Science in Business Analytics
+                        Program at the University of Southern California</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">Subash D'Souza</span>
+                    <span class="listing-feature__item-title">Founder, Data Con LA</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">ANITA SENGUPTA</span>
+                    <span class="listing-feature__item-title">Chief Product Officer / Co-Founder at at Airspace
+                        Experience Technologies</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">Subash D'Souza</span>
+                    <span class="listing-feature__item-title">Founder, Data Con LA</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">Subash D'Souza</span>
+                    <span class="listing-feature__item-title">Founder, Data Con LA</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">ANITA SENGUPTA</span>
+                    <span class="listing-feature__item-title">Chief Product Officer / Co-Founder at at Airspace
+                        Experience Technologies</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">Subash D'Souza</span>
+                    <span class="listing-feature__item-title">Founder, Data Con LA</span>
+                </div>
+                <div class="listing-feature__item">
+                    <span class="listing-feature__item-name">Subash D'Souza</span>
+                    <span class="listing-feature__item-title">Founder, Data Con LA</span>
+                </div>
+
+                <i class="listing-feature__item" aria-hidden="true"></i>
+                <i class="listing-feature__item" aria-hidden="true"></i>
             </div>
         </div>
     </div>
---
+++ design-panels.php
@@ -0,0 +1,35 @@
+<?php 
+
+/* Template Name: Design Test Panels */
+
+/**
+ * The template for displaying all landing page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Datacon_Refresh
+ */
+
+get_header();
+?>
+
+<main id="primary" class="main-content">
+
+    <div class="panels">
+        <h3 class="panels__header header">Keynote Speakers</h3>
+        <div class="panels__items-container">
+            <div class="panels__item">
+
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php
+get_footer();
