23c9a53efce5e0c49f39f274cae5815bd8d13f19
Neil Yonzon <neilyonzon@Lubbys-Laptop.lan>
2021-04-07 01:17:39 -0700

    Added Panels HTML and CSS

--- design-panels.php
+++ design-panels.php
@@ -20,12 +20,68 @@ get_header();
 
 <main id="primary" class="main-content">
 
-    <div class="panels">
-        <h3 class="panels__header header">Keynote Speakers</h3>
+    <div class="panels panels--primary" style="background-image:url(<?php echo THEME_IMG_PATH  ?>/data_graphic.jpg)">
+        <div class="panels__overlay"></div>
+        <h3 class="panels__header header header--primary">Panels</h3>
         <div class="panels__items-container">
             <div class="panels__item">
-
+                <h4 class="panels__item-title">Data 4 Good</h4>
+                <div class="panels__item-description">
+                    <p>
+                        Data 4 Good is an organization that was launched in 2019 with the promise of using data to help
+                        deal
+                        with social issues. Since it launched, it has attracted a public – private consortium of
+                        industry
+                        leaders as part of the advisory board.</p>
+                </div>
+                <a href="#" class="link--primary">Learn More</a>
             </div>
+            <div class="panels__item">
+                <h4 class="panels__item-title">Accessibility Challenges of 2020</h4>
+                <div class="panels__item-description">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    </p>
+                </div>
+                <a href="#" class="link--primary">Learn More</a>
+            </div>
+            <div class="panels__item">
+                <h4 class="panels__item-title">Think in Color</h4>
+                <div class="panels__item-description">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    </p>
+                </div>
+                <a href="#" class="link--primary">Learn More</a>
+            </div>
+            <div class="panels__item">
+                <h4 class="panels__item-title">Data 4 Good</h4>
+                <div class="panels__item-description">
+                    <p>
+                        Data 4 Good is an organization that was launched in 2019 with the promise of using data to help
+                        deal
+                        with social issues. Since it launched, it has attracted a public – private consortium of
+                        industry
+                        leaders as part of the advisory board.</p>
+                </div>
+                <a href="#" class="link--primary">Learn More</a>
+            </div>
+            <div class="panels__item">
+                <h4 class="panels__item-title">Data 4 Good</h4>
+                <div class="panels__item-description">
+                    <p>
+                        Data 4 Good is an organization that was launched in 2019 with the promise of using data to help
+                        deal
+                        with social issues. Since it launched, it has attracted a public – private consortium of
+                        industry
+                        leaders as part of the advisory board.</p>
+                </div>
+                <a href="#" class="link--primary">Learn More</a>
+            </div>
+            <i class="panels__item"></i>
+            <i class="panels__item"></i>
+            <i class="panels__item"></i>
         </div>
     </div>
 
